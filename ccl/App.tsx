ae22d84dbdcd35c6acbaa2bb1ad3b7a65fc151e8
ironman-eric <eonno123@yahoo.com>
2023-09-27 22:38:14 -0700

    good start

--- ccl/App.tsx
+++ ccl/App.tsx
@@ -19,11 +19,10 @@ import {
 
 import {
   Colors,
-  DebugInstructions,
   Header,
-  LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+
+import BookBarCodeReader from './components/BookBarCodeReader';
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -76,20 +75,7 @@ function App(): JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <BookBarCodeReader />
         </View>
       </ScrollView>
     </SafeAreaView>
