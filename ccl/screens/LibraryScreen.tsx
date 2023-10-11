8f3edd6971306cead0211ef25e13490514b44a69
ironman-eric <eonno123@yahoo.com>
2023-10-11 09:51:01 -0700

    Force list refresh when re-focussed on library screen

--- ccl/screens/LibraryScreen.tsx
+++ ccl/screens/LibraryScreen.tsx
@@ -1,11 +1,45 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useEffect, useState, useReducer } from 'react';
+import { StyleSheet, View, ActivityIndicator } from 'react-native';
 import ListBooks from '../components/library/ListBooks';
+import { useIsFocused } from '@react-navigation/native';
+import { Book, listBooks, deleteBook } from '../data/Book';
 
 const LibraryScreen = () => {
+  const isFocused = useIsFocused();
+  const [ignored, forceUpdate] = useReducer(x => x + 1, 0);
+
+  const [isLoading, setLoading] = useState(true);
+  const [books, setBooks] = useState<Book[]>();
+
+  useEffect(() => {
+    if (isFocused) {
+      setLoading(true);
+      forceUpdate();
+      listBooks()
+        .then(data => setBooks(data))
+        .catch(error => console.error(error))
+        .finally(() => setLoading(false));
+    }
+  }, [isFocused]);
+
+  const handleRemoveBook = (book: Book) => {
+    deleteBook(book)
+      .then(() => {
+        listBooks()
+          .then(data => setBooks(data))
+          .catch(error => console.error(error))
+          .finally(() => setLoading(false));
+      })
+      .catch(error => console.error(error))
+  };
+
   return (
     <View style={styles.container}>
-      <ListBooks />
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <ListBooks books={books} onRemoveBook={handleRemoveBook} />
+      )}
     </View>
   );
 };
